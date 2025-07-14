d581605cc6dfa7ec666ecdf44d9dc2741d789f44
Denys Halytskyi <denys.halytskyi17@gmail.com>
2025-07-15 01:05:08 +0300

    update file models.py, create class Transaction

--- Decrypto_DB/models.py
+++ Decrypto_DB/models.py
@@ -18,3 +18,11 @@ class Asset(models.Model):
     crypto = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE, related_name='assets')
     amount = models.DecimalField(max_digits=20, decimal_places=8)
 
+
+class Transaction(models.Model):
+    tx_id = models.CharField(max_length=255, unique=True)
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='transactions')
+    crypt = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE, related_name='transactions')
+    amount = models.DecimalField(max_digits=20, decimal_places=8)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    note = models.CharField(max_length=255,  blank=True)
