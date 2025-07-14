4d6d7360429c196801eb73ac94b54f8e620fea87
Denys Halytskyi <denys.halytskyi17@gmail.com>
2025-07-15 00:51:00 +0300

    update file models.py, create class Cryptocurrency

--- Decrypto_DB/models.py
+++ Decrypto_DB/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Cryptocurrency(models.Model):
+    symbol = models.CharField(max_length=10)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+
+
+
