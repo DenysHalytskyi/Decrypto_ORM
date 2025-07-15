cbfb709709d6a339d97ddf107d06a3471f422124
Denys Halytskyi <denys.halytskyi17@gmail.com>
2025-07-15 15:37:23 +0300

    unboxing wallets.json in main.py and enter data in cryptocurrency

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import json
+from tkinter.font import names
+
+from Decrypto_DB.models import Cryptocurrency, Wallet, Asset, Transaction
 import init_django_orm
 
+
+def main():
+
+    with open("wallets.json", "r") as file:
+        wallets = json.load(file)
+
+        cryptos = wallets.get("crypto", [])
+        for crypto in cryptos:
+            Cryptocurrency.objects.get_or_create(
+                symbol=crypto["symbol"],
+                name=crypto["name"],
+                defaults={"description": crypto.get("description", "")}
+            )
 
 
 
